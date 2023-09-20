a59e7378a997c158dcbd486e8d163086c25f5917
kimsojeong <rtw0202@naver.com>
2023-09-20 21:06:54 +0900

    컴알 과제 1

--- quick_sort_lib.py
+++ quick_sort_lib.py
@@ -1,20 +1,30 @@
 def quick_sort(A):
     def sort(lo, hi):
-        if lo >= hi: return  # 이미 정렬이 된 경우, 함수를 종료함
+        if lo >= hi:
+            return
+
         mid = partition(lo, hi)
         sort(lo, mid - 1)
         sort(mid + 1, hi)
 
     def partition(lo, hi):
         pivot = A[lo]  # 첫번째 원소를 pivot으로 삼음
-        while lo <= hi:
-            while A[lo] < pivot:
-                lo += 1
-            while A[hi] > pivot:
-                hi -= 1
-            if lo <= hi:
-                A[lo], A[hi] = A[hi], A[lo]
-                lo, hi = lo + 1, hi - 1
-        return lo
+        left = lo + 1
+        right = hi
 
-    return sort(0, len(A) - 1)
+        while left <= right:
+            while left <= hi and A[left] <= pivot:  # left <= pivot가 될 때까지 left 증가
+                left += 1
+            while A[right] >= pivot and right >= left:  # pivot <= hi가 될 때까지 right 감소
+                right -= 1
+            if right < left:
+                break
+            else:
+                A[left], A[right] = A[right], A[left]
+
+        A[lo], A[right] = A[right], A[lo]
+
+        return right
+
+    sort(0, len(A) - 1)
+
